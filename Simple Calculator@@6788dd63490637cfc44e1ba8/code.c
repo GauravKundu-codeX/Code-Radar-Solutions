9af9dce92fe928178d98feed01da109aaa1abe70
GauravKundu-codeX <gaurav0283.becse24@chitkara.edu.in>
2025-02-11 12:15:16 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+
+int main() {
+    int a,b;
+    char c;
+    scanf("%d" "%d" "%c",&a,&b,&c);
+    if(c=='+'){
+        print("%d",a+b);
+    }
+    else if(c=='-'){
+        print("%d",a-b);
+    }
+    else if(c=='*'){
+        print("%d",a*b);
+    }
+    else if(c=='/'){
+       if(b==0){
+        printf("Invalid");
+       }
+       else{
+        printf("%d",a/b);
+       }
+    }
+    return 0;
+}
